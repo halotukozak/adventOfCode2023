cd38d573bf84df6b9ab8625460d07df577cd3f23
bartlomiejkozak <bartlomiejkozak@proton.me>
2023-12-04 18:59:06 +0100

    nit

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,20 +2,22 @@ import kotlin.math.pow
 
 fun main() {
 
-  fun part1(input: List<String>): Int = input.fold(0) { acc, line ->
-    acc + 2.pow(line.split(':', '|').let {
-      it.drop(1).map { it.split(' ').filter(String::isNotEmpty) }
-    }.let {
-      it[0].intersect(it[1]).size - 1
-    })
-  }
+  fun part1(input: List<String>): Int =
+    input.fold(0) { acc, line ->
+      acc + 2.pow(line.split(':', '|').let {
+        it.drop(1).map { it.split(' ').filter(String::isNotEmpty) }
+      }.let {
+        it[0].intersect(it[1]).size - 1
+      })
+    }
 
-  fun part2(input: List<String>): Int = input.foldIndexed(List(input.size) { 1 }) { row, acc, line ->
-    line.split(':', '|')
-      .let { it.drop(1).map { it.split(' ').filter(String::isNotEmpty) } }
-      .let { it[0].intersect(it[1]).size }
-      .let { wonCards -> acc.mapIndexed { i, n -> n + if (row < i && i <= row + wonCards) acc[row] else 0 } }
-  }.sum()
+  fun part2(input: List<String>): Int =
+    input.foldIndexed(List(input.size) { 1 }) { row, acc, line ->
+      line.split(':', '|')
+        .let { it.drop(1).map { it.split(' ').filter(String::isNotEmpty) } }
+        .let { it[0].intersect(it[1]).size }
+        .let { wonCards -> acc.mapIndexed { i, n -> n + if (row < i && i <= row + wonCards) acc[row] else 0 } }
+    }.sum()
 
 
   val day = "Day04"
